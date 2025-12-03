5fbb0e964720db28d961378317b3093167be7ec0
Yojan Parajuli <yojanparajuli62@gmail.com>
2025-12-03 22:37:38 +0000

    For start of video

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -41,7 +41,7 @@ class TestUnit(unittest.TestCase):
     def test_rainy_classification_output(self):
         test_input = np.array([279.626,998,99,1,314,0.3,0,0,88]).reshape(1,-1)
         class_result, _ = classify_weather(test_input) 
-        self.assertEqual(class_result, 'rain')
+        self.assertEqual(class_result, 'rainy')
 
     def test_cloudy_classification_output(self):
         test_input = np.array([289.47,1015,88,2,300,0,0,0,20]).reshape(1,-1)
